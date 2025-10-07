e760f3579d4cf1294add5491e7f23cef1ce3af6c
ekta-developer <eekta.srivastava2000@gmail.com>
2025-10-07 09:50:16 +0530

    some small changes in the project
    some small changes in the project

--- Employee/demotask/src/constants/Constant.jsx
+++ Employee/demotask/src/constants/Constant.jsx
@@ -1,3 +1,6 @@
-export const BASE_URL = "http://192.168.23.17:3002/api/"; //? Shweta
+// export const BASE_URL = "http://192.168.23.17:3002/api/"; //? Shweta
+export const BASE_URL = "https://gens.demoquaeretech.in/api/api/"; //? Shweta
+// export const BASE_URL = "https://1gens.demoquaeretech.in/"; //? Shweta
+
 export const BASE_URL_IMG = "http://192.168.23.3002/"; //shweta
 export const BASE_URL_two = "http://192.168.23.13:8000/" //Abhishek Sir
--- Employee/demotask/vite.config.js
+++ Employee/demotask/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,          // Exposes to network (0.0.0.0)
+    port: 5599,
+  },
   plugins: [react()],
+ 
+  base: '/',   // 👈 important
+ 
+ 
 })

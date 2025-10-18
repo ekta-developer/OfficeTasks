7c3da667366fe9e7e629ad202d80e7c7e73a86a4
ekta-developer <eekta.srivastava2000@gmail.com>
2025-10-18 18:19:05 +0530

    Added different bg image
    Added different bg image

--- Employee/demotask/src/App.jsx
+++ Employee/demotask/src/App.jsx
@@ -1,15 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import "./AppCss.css";
 import "./MediaQuery.css";
 import AppRouter from "./router/AppRouter";
+import { CircularProgress } from "@mui/material";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(true);
 
-  return <AppRouter />;
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh", // full viewport height
+            width: "100%", // full width
+          }}
+        >
+          <CircularProgress color="inherit" />
+        </div>
+      ) : (
+        <>
+          <AppRouter />;
+        </>
+      )}
+    </>
+  );
 }
 
 export default App;
---
+++ Script.js
@@ -0,0 +1,5 @@
+// load loader on page refreash
+window.onload = function () {
+  document.getElementById("loader-wrapper").style.display = "none";
+  document.body.style.visibility = "visible";
+};
